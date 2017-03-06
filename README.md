a9d098ce630af6b187067ae5a0291ff9215d00c6
Carey Li <careyliau@gmail.com>
2017-03-07 03:47:48 +1100

    Initial working commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #Certificate Transparency watcher
+
+Requires Node >6.00 for ES6 features.
+
+## Usage:
+
+	./ctwatch discover twillo.com
--- index.js
+++
@@ -1,7 +0,0 @@
-/*
- * CTWatch
- * Desc: Searches Certificate Transparency services for certs w/ option to notify on new certs detected
- * Author: https://github.com/cry
- */
-
-require("lib/ct/googleCT.js");
